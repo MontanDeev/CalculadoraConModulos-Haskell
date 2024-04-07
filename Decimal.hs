e383421b27b2044b436bd6e561326df2e0da0930
MOISES SAMUEL LOPEZ BARRALES <141092089+MontanDeev@users.noreply.github.com>
2024-04-07 14:41:23 -0600

    Add files via upload

---
+++ Decimal.hs
@@ -0,0 +1,10 @@
+module Calculadora.Decimal(
+    sumar,
+    restar
+) where
+
+    sumar :: Double -> Double -> Double
+    sumar n m = n + m
+    
+    restar :: Double -> Double -> Double
+    restar n m = n - m
---
+++ Entero.hs
@@ -0,0 +1,10 @@
+module Calculadora.Entero(
+    sumar,
+    restar
+) where
+
+    sumar :: Integer-> Integer -> Integer
+    sumar n m = n + m
+    
+    restar :: Integer -> Integer -> Integer
+    restar n m = n - m
